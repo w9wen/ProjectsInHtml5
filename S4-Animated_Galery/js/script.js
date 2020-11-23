c91665eabd4d4132d558c65cc2afb8ab4dc280b0
William (NB) <william.wen@gmail.com>
2020-11-23 08:59:54 +0800

    [S4] Write the javascript.

--- S4-Animated_Galery/js/script.js
+++ S4-Animated_Galery/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   var items = $("#gallery li");
-  var itemsByTags = [];
+  var itemsByTags = {};
 
   // Loop through tags
   items.each(function (i) {
@@ -9,5 +9,57 @@ $(document).ready(function () {
 
     // Add data attribute for quicksand
     elem.attr("data-id", i);
+    $.each(tags, function (key, value) {
+      // Remove whitespace.
+      value = $.trim(value);
+
+      if (!(value in itemsByTags)) {
+        //// Add empty value
+        itemsByTags[value] = [];
+      }
+
+      //// Add image to array
+      itemsByTags[value].push(elem);
+    });
   });
+
+  ////Create "All Items" option.
+  createList("All Items", items);
+
+  $.each(itemsByTags, function (k, v) {
+    createList(k, v);
+  });
+
+  //// Click Handler
+  $("#navbar a").on("click", function (e) {
+    var link = $(this);
+
+    //Add active class
+    link.addClass("active").siblings().removeClass("active");
+
+    $("#gallery").quicksand(link.data("list").find("li"));
+    e.preventDefault();
+  });
+
+  $("#navbar :first").click();
+
+  //// Create the lists.active
+  function createList(text, items) {
+    //// Create empty ul
+    var ul = $("<ul>", { class: "hidden" });
+
+    $.each(items, function () {
+      $(this).clone().appendTo(ul);
+    });
+
+    //// Add gallery div
+    ul.appendTo("#gallery");
+
+    //// Create menu item
+    var a = $("<a>", {
+      html: text,
+      href: "#",
+      data: { list: ul },
+    }).appendTo("#navbar");
+  }
 });
