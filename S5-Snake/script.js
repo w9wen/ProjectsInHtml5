1b645499415a35fe73a31c0357467203e7b8abb6
William (NB) <william.wen@gmail.com>
2020-12-02 08:23:38 +0800

    [S5] Snake Game script

--- S5-Snake/script.js
+++ S5-Snake/script.js
@@ -1,3 +1,42 @@
 $(document).ready(function () {
-  alert(1);
+  var canvas = $("#canvas")[0];
+  var ctx = canvas.getContext("2d");
+  var w = canvas.width();
+  var h = canvas.height();
+  var cw = 15;
+  var d = "right";
+  var food;
+  var score;
+  var speed = 130;
+
+  // Snake Array
+  var snake_array;
+
+  // Initializer
+  function init() {
+    create_snake();
+    create_food();
+    score = 0;
+
+    if (typeof game_loop != "undefined") clearInterval(game_loop);
+    game_loop = setInterval(paint, speed);
+  }
+
+  init(); // Run Initializer
+
+  // Create Snake
+  function create_snake() {
+    var length = 5;
+    snake_array = [];
+    for (let index = length - 1; index >= 0; index--) {
+      snake_array.push({ x: index, y: 0 });
+    }
+  }
+
+  function create_food() {
+    food = {
+      x: Math.round((Math.random() * (w - cw)) / cw),
+      y: Math.round((Math.random() * (h - cw)) / cw),
+    };
+  }
 });
