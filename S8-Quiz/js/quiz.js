06227cf635149c3dcc6687adf86a2c546fc24493
William (NB) <william.wen@gmail.com>
2020-12-29 08:41:40 +0800

    [S8

--- S8-Quiz/js/quiz.js
+++ S8-Quiz/js/quiz.js
@@ -23,34 +23,78 @@ $(document).ready(function () {
 
   $("#q1 #submit").click(function () {
     $(".questionForm").hide();
+    process("q1");
     $("#q2").fadeIn(300);
     return false;
   });
 
   $("#q2 #submit").click(function () {
     $(".questionForm").hide();
+    process("q2");
     $("#q3").fadeIn(300);
     return false;
   });
 
   $("#q3 #submit").click(function () {
     $(".questionForm").hide();
+    process("q3");
     $("#q4").fadeIn(300);
     return false;
   });
 
   $("#q4 #submit").click(function () {
     $(".questionForm").hide();
+    process("q4");
     $("#q5").fadeIn(300);
     return false;
   });
 
   $("#q5 #submit").click(function () {
     $(".questionForm").hide();
+    process("q5");
     $("#result").fadeIn(300);
     return false;
   });
 });
+
+//// Process the answers
+function process(q) {
+  if (q == "q1") {
+    var submitted = $("input[name=q1]:checked").val();
+    if (submitted == sessionStorage.a1) {
+      score++;
+    }
+  }
+  if (q == "q2") {
+    var submitted = $("input[name=q2]:checked").val();
+    if (submitted == sessionStorage.a2) {
+      score++;
+    }
+  }
+  if (q == "q3") {
+    var submitted = $("input[name=q3]:checked").val();
+    if (submitted == sessionStorage.a3) {
+      score++;
+    }
+  }
+  if (q == "q4") {
+    var submitted = $("input[name=q4]:checked").val();
+    if (submitted == sessionStorage.a4) {
+      score++;
+    }
+  }
+  if (q == "q5") {
+    var submitted = $("input[name=q5]:checked").val();
+    if (submitted == sessionStorage.a5) {
+      score++;
+    }
+    $("#result").html(
+      "<h3>Your final score is: " +
+        score +
+        'out of 5</h3><a href="index.html">Take Quiz Again</a>'
+    );
+  }
+}
 
 //// Add event listener
 window.addEventListener("load", init, false);
