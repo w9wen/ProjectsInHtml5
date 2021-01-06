23fc2d84f33e4d7b4598f69ca1138859cc663135
William (NB) <william.wen@gmail.com>
2021-01-06 08:33:29 +0800

    [S9] Click button handler.

--- S9-AudioPlayer/js/main.js
+++ S9-AudioPlayer/js/main.js
@@ -1,7 +1,29 @@
-var audio = new Audio("media/Linkin Park - Papercut.mp3");
+var audio;
 
-//// Hide Pause
 $("#pause").hide();
+
+initAudio($("#playlist li:first-child"));
+
+function initAudio(element) {
+  var song = element.attr("song");
+  var title = element.text();
+  var cover = element.attr("cover");
+  var artist = element.attr("artist");
+
+  //// Create audio object
+  audio = new Audio("media/" + song);
+
+  //// Insert audio info
+  $(".artist").text(artist);
+  $(".title").text(title);
+
+  //// Insert song cover
+  $("img.cover").attr("src", "images/covers/" + cover);
+
+  $("#playlist li").removeClass("active");
+  element.addClass("active");
+}
+//// Hide Pause
 
 //// Play button
 $("#play").click(function () {
@@ -24,6 +46,40 @@ $("#stop").click(function () {
   audio.currentTime = 0;
   //   $("#play").show();
   //   $("#pause").hide();
+});
+
+//// Next button
+$("#next").click(function () {
+  audio.pause();
+  var next = $("#playlist li.active").next();
+  if (next.length == 0) {
+    next = $("#playlist li:first-child");
+  }
+  initAudio(next);
+  audio.play();
+  showDuration();
+});
+
+//// Prey button
+$("#prev").click(function () {
+  audio.pause();
+  var prev = $("#playlist li.active").prev();
+  if (prev.length == 0) {
+    prev = $("#playlist li:last-child");
+  }
+  initAudio(prev);
+  audio.play();
+  showDuration();
+});
+
+//// Playlist song click
+$("#playlist li").click(function () {
+  audio.pause();
+  initAudio($(this));
+  $("#play").hide();
+  $("#pause").show();
+  audio.play();
+  showDuration();
 });
 
 //// Volume control
