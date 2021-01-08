f44a0632a12281b1bfb9e9e95b21187361be504e
William (NB) <william.wen@gmail.com>
2021-01-08 18:06:47 +0800

    [S10] Display todo.

--- S10-Mobile/js/main.js
+++ S10-Mobile/js/main.js
@@ -1,4 +1,35 @@
+//// Set todo list variable
+var todoList = JSON.parse(localStorage.getItem("todos"));
+
 $(document).ready(function () {
+  //// Set counter
+  var i = 0;
+
+  //// Check for todos
+  if (todoList != null) {
+    //// Loop through and output li items
+    $.each(todoList, function (key, value) {
+      $("#todos").prepend(
+        '<li id="task-' +
+          i +
+          '"><a id="todo_link" href="#edit" data-todo_name ="' +
+          value.todo_name +
+          '" data-todo_date="' +
+          value.todo_date +
+          '">' +
+          value.todo_name +
+          " <span>" +
+          value.todo_date +
+          "</span></a></li>"
+      );
+
+      i++;
+    });
+    //// Refresh
+    $("#todos").listview("refresh");
+  }
+
+  //// Add Todo
   $("#add_form").submit(function (params) {
     //// Get submitted values
     var todo_name = $("#todo_name").val();
@@ -26,5 +57,9 @@ $(document).ready(function () {
       localStorage.setItem("todos", JSON.stringify(todos));
     }
   });
+
+  //// Clear Todos
+  $("#clear_button").click(function () {
+    localStorage.clear();
+  });
 });
-//// Add Todo
